c874791e035cbb434b0bc686ab8f315adf6b264a
datlp6 <datlp6@fpt.com.vn>
2020-06-25 14:56:56 +0700

    update

--- face_detector/views.py
+++ face_detector/views.py
@@ -8,6 +8,7 @@ import urllib.request as urllib
 import json
 import cv2
 import os
+from django.http import HttpResponse
 # define the path to the face detector
 FACE_DETECTOR_PATH = "{base_path}/cascades/haarcascade_frontalface_default.xml".format(
 	base_path=os.path.abspath(os.path.dirname(__file__)))
@@ -44,8 +45,14 @@ def detect(request):
 	rects = [(int(x), int(y), int(x + w), int(y + h)) for (x, y, w, h) in rects]
 	# update the data dictionary with the faces detected
 	data.update({"num_faces": len(rects), "faces": rects, "success": True})
+	# vẽ hình chữ nhật
+	for (startX, startY, endX, endY) in rects:
+		cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+	# lưu hình lại
+	cv2.imwrite('django_project/static/out.png', image)
+	return HttpResponse('<img src="/static/out.gif" />')
 	# return a JSON response
-	return JsonResponse(data)
+	# return JsonResponse(data)
 def _grab_image(path=None, stream=None, url=None):
 	# if the path is not None, then load the image from disk
 	if path is not None:
